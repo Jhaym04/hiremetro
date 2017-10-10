90ecd225cd92359cfc7ae811b791a6f653f4538c
Jhaym04 <31693927+Jhaym04@users.noreply.github.com>
2017-10-10 09:26:36 +0800

    FINAL ! for now ..

--- application/views/hiremetro/ads.php
+++ application/views/hiremetro/ads.php
@@ -15,173 +15,31 @@
 		<div class="collapse navbar-collapse" id="myNavbar">
 		
 			<ul class="nav navbar-nav navbar-right">
-				<li><a data-toggle="modal" data-target="#signup1Modal" style="cursor: pointer;"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-				<li><a data-toggle="modal" data-target="#loginModal" style="margin-right: 20px; cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+				<?php
+					
+					if($this->session->userdata('logged_in') == TRUE){
+						echo '
+						<li><a href="'; echo base_url('hiremetro'); echo '"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+						<li class="dropdown">
+							<a href="#"  style="margin-right: 20px;" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> '.$this->session->userdata('username').' </a>
+							<ul class="dropdown-menu">
+								<li><a href="'; echo base_url('hiremetro/view_profile'); echo '">Profile <span style="color: black;" class="glyphicon glyphicon-user"></span></a></li>
+								<li><a href="'; echo base_url('hiremetro/logout'); echo'"> Logout <span style="color: black;" class="glyphicon glyphicon-off"></span></a></li>
+							</ul>
+						</li>';
+					}else{
+						echo '
+						<li><a href="'; echo base_url('hiremetro'); echo '"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+						<li><a data-toggle="modal" data-target="#signup1Modal" style="cursor: pointer;"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+						<li><a data-toggle="modal" data-target="#loginModal" style="margin-right: 20px; cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> ';
+					};
+				?>
 			</ul>
 		</div>
 	</nav>
-	
-	<!-- Modal for login-->
-		<div class="modal fade" id="loginFALSE" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content" id="login">
-					<div class="modal-header text-center" id="header">
-						<button type="button" class="close" data-dismiss="modal"> &times; </button>
-						<h4 class="modal-title"> LOGIN </h4>
-						<p>Attention</p>
-					</div>
-					<div class="modal-body text-center">
-						<p>Wrong username or password !! Try again !</p>
-					</div>
-					<div class="modal-footer text-center">
-						<button class="btn btn-default" data-dismiss="modal">Ok</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	
-	<!-- Modal for wrong login-->
-		<div class="modal fade" id="loginModal" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content" id="login">
-					<div class="modal-header text-center" id="header">
-						<button type="button" class="close" data-dismiss="modal"> &times; </button>
-						<h4 class="modal-title"> LOGIN </h4>
-						<p>check and update your account</p>
-					</div>
-					<div class="modal-body">
-						<form action="<?php echo base_url('hiremetro/login')?>" method="post">
-							<div class="form-vertical">
-								<div class="form-group">
-									<input type="text" class="form-control" name="username" placeholder="Username"> 
-								</div>
-								<div class="form-group">
-									<input type="password" class="form-control" name="password" placeholder="Password"> 
-								</div>
-								<br />
-								<div class="text-center">
-									<a href=""> Forgot Password </a>
-								</div>
-							</div>
-					</div>
-					<div class="modal-footer text-center">
-						<button type="submit" class="btn btn-default">Submit</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	
-	<!-- Modal for signup 1-->
-		<div class="modal fade" id="signup1Modal" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content" id="login">
-					<div class="modal-header text-center" id="header">
-						<button type="button" class="close" data-dismiss="modal"> &times; </button>
-						<h4 class="modal-title"> SIGN UP </h4>
-						<p>become an employee</p>
-					</div>
-					<div class="modal-body">
-						<form action="<?php echo base_url('hiremetro/signup1')?>" method="post">
-							<div class="form-vertical">
-								<div class="form-group form-inline" id="signup_name">
-									<input type="text" class="form-control" name="firstname" placeholder="First name"> 
-									<input type="text" class="form-control" name="middlename" placeholder="Middle Initial"> 
-									<input type="text" class="form-control" name="lastname" placeholder="Last name"> 
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" name="address" placeholder="Address"> 
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" name="contact_number" placeholder="Mobile number"> 
-								</div>
-								<div class="form-group">
-									<input type="email" class="form-control" name="email" placeholder="Email"> 
-								</div>
-								<div class="form-group">
-									<div class="input-group date" data-provide="datepicker">
-										<input type="text" class="form-control" name="birthday" placeholder="Birthday">
-										<div class="input-group-addon">
-											<span class="glyphicon glyphicon-th"></span>
-										</div>
-										
-										<script>
-											$('.datepicker').datepicker();
-										</script>
-										
-									</div>
-								</div>
-								<div class="form-group form-inline">
-									<label class="radio-inline">
-										<input type="radio" name="sex" value="Male" checked >Male
-									</label>
-									<label class="radio-inline">
-										 <input type="radio" name="sex" value="Female">Female
-									</label>
-								</div>
-							</div>
-					</div>
-					<div class="modal-footer text-center">
-						<button type="submit" class="btn btn-default" name="signup" value="1">Next</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
+
 		
-	<!-- Modal for signup 2-->
-		<div class="modal fade" id="signup2Modal" role="dialog">
-			<div class="modal-dialog">
-				<div class="modal-content" id="login">
-					<div class="modal-header text-center" id="header">
-						<button type="button" class="close" data-dismiss="modal"> &times; </button>
-						<h4 class="modal-title"> SIGN UP </h4>
-						<p>Enter your work details !</p>
-					</div>
-					<div class="modal-body">
-						<form action="<?php echo base_url('hiremetro/signup2')?>" method="post">
-							<div class="form-vertical">
-								<div class="form-group form-inline">
-									<input type="text" class="form-control" name="username" placeholder="Username"> 
-									<input type="password" class="form-control" name="password" placeholder="Password">  
-								</div>
-								<div class="form-group">
-								  <label for="sel1">Choose Work Category</label>
-								  <select class="form-control" id="sel1" name="work_title">
-									<option>Bartender</option>
-									<option>Carpenter</option>
-									<option>Cook</option>
-									<option>Driver</option>
-									<option>Gardener</option>
-									<option>Janitor</option>
-									<option>Maid</option>
-									<option>Masseuse</option>
-									<option>Nanny</option>
-									<option>Plumber</option>
-									<option>Tutor</option>
-									<option>Waiter</option>
-								  </select>
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" name="work_description" placeholder="Work Description"> 
-								</div>
-								<div class="form-group">
-									<input type="number" class="form-control" name="work_pay" placeholder="Work Pay"> 
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" name="work_location" placeholder="Service Location"> 
-								</div>
-							</div>
-					</div>
-					<div class="modal-footer text-center">
-						<button type="submit" class="btn btn-default" name="back" value="0">Submit</button>
-						<button type="submit" class="btn btn-default" name="back" value="1">Back</button>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
+	
 	
 	
 	<div class="container-fluid pagee" style="background-image: url('images/bg.png'); min-height: 500px; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; ">
@@ -209,8 +67,18 @@
 					</div>
 					
 					<div class="col-md-3 text-center" >
-						<h2>Lei Dian Mendoza</h2>
-						<h2 style="color:Teal">CARPENTER</h4>
+						<h2>
+							<?php foreach((array)$employees as $e) {
+							echo ''.$e['lname'].', '.$e['fname'].' '.$e['mname'].'';
+							}
+							?>
+						</h2>
+						<h2 style="color:Teal">
+							<?php foreach((array)$employees as $e) {
+							echo ''.$e['work_title'].'';
+							}
+							?>
+						</h4>
 						
 					</div>
 					
@@ -234,7 +102,10 @@
 			</div>
 			<div class="col-md-6 text-center">
 			<p>
-			Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				<?php foreach((array)$employees as $e) {
+					echo ''.$e['work_description'].'';
+					}
+				?>
 			</p>
 			</div>
 			<div class="col-md-3">
@@ -269,14 +140,39 @@
 						<div class="col-md-1">
 						</div>
 						<div class="col-md-4">
-							<h4><span class="glyphicon glyphicon-envelope"></span> ldtm17gmail.com</p>
-							<h4><span class="glyphicon glyphicon-map-marker red"></span> Dasmarinas, Cavite</p>
-							<h4><span class="glyphicon glyphicon-phone"></span> 09367935476</p>	
+							<h4><span class="glyphicon glyphicon-envelope"></span>
+								<?php foreach((array)$employees as $e) {
+								echo ''.$e['email'].'';
+								}
+								?>
+							</p>
+							<h4><span class="glyphicon glyphicon-map-marker red"></span> 
+								<?php foreach((array)$employees as $e) {
+								echo ''.$e['worker_location'].'';
+								}
+								?>
+							</p>
+							<h4><span class="glyphicon glyphicon-phone"></span>
+								<?php foreach((array)$employees as $e) {
+								echo ''.$e['contact'].'';
+								}
+								?>
+							</p>	
 						</div>
 						<div class="col-md-4">
-							<h4>Birthday: October 17, 1999</h4>
-							<h4>Sex: Female</h4>
-							<h4>Languages Spoken: Filipino</h4>
+							<h4>Birthday: <?php foreach((array)$employees as $e) {
+								echo ''.$e['birthday'].'';
+								}
+								?>
+							</h4>
+							<h4>Sex: <?php foreach((array)$employees as $e) {
+								echo ''.$e['sex'].'';
+								}
+								?>
+							</h4>
+							<h4>Languages Spoken: 
+							</h4>
+							
 						</div>
 						<div class="col-md-3">
 							<div class="row">
--- application/views/hiremetro/search.php
+++ application/views/hiremetro/search.php
@@ -42,8 +42,25 @@
 			
 		<div class="collapse navbar-collapse" id="myNavbar">
 			<ul class="nav navbar-nav navbar-right">
-				<li><a href="<?php echo base_url('hiremetro') ?>"><span class="glyphicon glyphicon-home"></span> Home</a></li>
-				<li><a href="#" style="margin-right: 20px;"><span class="glyphicon glyphicon-user"></span> Account Name</a></li>
+				<?php
+					
+					if($this->session->userdata('logged_in') == TRUE){
+						echo '
+						<li><a href="'; echo base_url('hiremetro'); echo '"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+						<li class="dropdown">
+							<a href="#"  style="margin-right: 20px;" class="dropdown-toggle" data-toggle="dropdown"><span class="glyphicon glyphicon-user"></span> '.$this->session->userdata('username').' </a>
+							<ul class="dropdown-menu">
+								<li><a href="'; echo base_url('hiremetro/view_profile'); echo '">Profile <span style="color: black;" class="glyphicon glyphicon-user"></span></a></li>
+								<li><a href="'; echo base_url('hiremetro/logout'); echo'"> Logout <span style="color: black;" class="glyphicon glyphicon-off"></span></a></li>
+							</ul>
+						</li>';
+					}else{
+						echo '
+						<li><a href="'; echo base_url('hiremetro'); echo '"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+						<li><a data-toggle="modal" data-target="#signup1Modal" style="cursor: pointer;"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+						<li><a data-toggle="modal" data-target="#loginModal" style="margin-right: 20px; cursor: pointer;"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> ';
+					};
+				?>
 			</ul>
 		</div>
 	</nav>
@@ -90,9 +107,9 @@
 							</div>		
 								
 							<div class="col-md-7">
-								<a href=""><h4>'.$e['lname'].', '.$e['fname'].' '.$e['mname'].'</h4></a>
+								<a href="'; echo base_url('hiremetro/employee_profile').'?username='.$e['username'].'"><h4>'.$e['lname'].', '.$e['fname'].' '.$e['mname'].'</h4></a>
 								<h5> '.$e['address'].' | '.$e['sex'].' | '.$dob.' yrs old </h5>
-								<h6> '.$e['description'].'</h6>
+								<h6> '.$e['description'].'	</h6>
 							</div>
 								
 							<div class="col-md-3">
