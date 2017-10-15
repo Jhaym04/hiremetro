ef0801f89bb44ad986cef4272097202463a2a7f0
ecmariin <31694078+ecmariin@users.noreply.github.com>
2017-10-15 17:50:49 +0800

    Admin Report view

---
+++ Elvin(commits)/admin_reports.php
@@ -0,0 +1,172 @@
+<div class="container-fluid pagee" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+	
+	<br/>
+	<br/>
+	<br/>
+	
+	<div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+			<br/>
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-th-list"> Dashboard</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"> Profiles</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"> Reports</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-check"> Suggestions</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-wrench"> Settings</a></li>
+			
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			
+			<?php //<p> hiremetro © 2017 | Designed by Group 4 </p>  ?>
+			<div class="row">
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+					<h6 style="color: white;">hiremetro © 2017 | Designed by Group 4</h6>
+				</div>
+				<div class="col-sm-1">
+				</div>
+			</div>
+          </ul>
+		</div>
+	</div>
+	
+	<div class="row" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-10" >
+			<div class="row">
+				<div class="col-md-11">
+					<br />
+					<div class="row">
+						<div class="col-md-1">
+							<img class="img-responsive" src="<?php echo base_url('images/reports_logo.png')?>" style="opacity:0.75;" >
+						</div>
+						<div class="col-md-10">
+							<h1>Reports</h1>
+						</div>
+					</div>
+					
+					<hr style="border: solid 2px black; opacity:0.50;"/>
+					
+					<div class="row">
+						<div class="col-md-6">
+							<table class="table table-responsive table-hover">
+								<thead>
+								  <tr>
+									<th>Name</th>
+									<th>Occupation</th>
+									<th># of Reports</th>
+									<th>Actions</th>
+								  </tr>
+								</thead>
+								<tbody>
+									<tr class="active">
+										<td>Celine Joy Macaraeg</td>
+										<td>Maid</td>
+										<td>4</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>
+									<tr class="info">
+										<td>Czar Joshua Mamawan</td>
+										<td>Driver</td>
+										<td>1</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>
+									<tr class="info">
+										<td>John Mher Dhexs Emotin</td>
+										<td>Carpenter</td>
+										<td>1</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>									  
+								</tbody>
+							</table>
+						</div>
+						<div class="col-md-6">
+							<table class="table table-responsive table-hover">
+								<thead>
+								  <tr>
+									<th>Reports</th>
+									</tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>
+											<div class="card">
+												<div class="card-block px-3">
+													<h4 class="card-title">Subject Line(example masungit)</h4>
+													<p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+													<a href="#" class="btn btn-primary">Read More</a>
+													<p class="card-text" style="text-align:right">10-15-2017</p>
+												</div>
+											</div>	
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<div class="card">
+												<div class="card-block px-3">
+													<h4 class="card-title">Subject Line(example masungit)</h4>
+													<p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+													<a href="#" class="btn btn-primary">Read More</a>
+													<p class="card-text" style="text-align:right">10-15-2017</p>
+												</div>
+											</div>	
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<div class="card">
+												<div class="card-block px-3">
+													<h4 class="card-title">Subject Line(example masungit)</h4>
+													<p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+													<a href="#" class="btn btn-primary">Read More</a>
+													<p class="card-text" style="text-align:right">10-15-2017</p>
+												</div>
+											</div>	
+										</td>
+									</tr>
+									<tr>
+										<td>
+											<div class="card">
+												<div class="card-block px-3">
+													<h4 class="card-title">Subject Line(example masungit)</h4>
+													<p class="card-text">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+													<a href="#" class="btn btn-primary">Read More</a>
+													<p class="card-text" style="text-align:right">10-15-2017</p>
+												</div>
+											</div>	
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+					</div>
+					
+					<br />
+					<br />
+				</div>
+				
+				<div class="col-md-1">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+</div>
