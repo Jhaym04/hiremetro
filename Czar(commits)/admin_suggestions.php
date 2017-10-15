2e2597e574b4b53f95b07568afc4ebeac184a846
maw0904 <31690512+rarkzem@users.noreply.github.com>
2017-10-15 20:56:55 +0800

    Updated Admin Suggestions View with Pagination

---
+++ Czar(commits)/admin_suggestions.php
@@ -0,0 +1,182 @@
+ <div class="container-fluid pagee" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+	
+	<br/>
+	<br/>
+	<br/>
+	
+	<div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+			<br/>
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-th-list"> Dashboard</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"> Profiles</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"> Reports</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-check"> Suggestions</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-wrench"> Settings</a></li>
+			
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			
+			<?php //<p> hiremetro © 2017 | Designed by Group 4 </p>  ?>
+			<div class="row">
+				<div class="col-sm-1">
+				</div>
+				<div class="col-sm-10">
+					<h6 style="color: white;">hiremetro © 2017 | Designed by Group 4</h6>
+				</div>
+				<div class="col-sm-1">
+				</div>
+			</div>
+          </ul>
+		</div>
+	</div>
+	
+	<div class="row" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-10" >
+			<div class="row">
+				<div class="col-md-11">
+					<br />
+					<div class="row">
+						<div class="col-md-1">
+							<img class="img-responsive" src="<?php echo base_url('images/suggestions_logo.png')?>" style="opacity:0.75;" >
+						</div>
+						<div class="col-md-10">
+							<h1>Suggestions</h1>
+						</div>
+					</div>
+					
+					<hr style="border: solid 2px black; opacity:0.50;"/>
+					
+					<table class="table table-responsive table-hover">
+						<thead>
+						  <tr>
+							<th>Subject</th>
+							<th>Suggestion</th>
+							<th>Date</th>
+							<th>Actions</th>
+						  </tr>
+						</thead>
+						<tbody>
+						  
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						  <tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+							<tr class="info">
+							<td>Subject Line</td>
+							<td>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</td>
+							<td>10-15-2017</td>
+							<td>
+								<a href="">View</a> |
+								<a href="">Delete</a>
+							</td>
+						  </tr>
+						</tbody>
+					  </table>
+					<nav aria-label="Page navigation example">
+					  <ul class="pagination justify-content-end">
+						<li class="page-item disabled">
+						  <a class="page-link" href="#" tabindex="-1">Previous</a>
+						</li>
+						<li class="page-item"><a class="page-link" href="#">1</a></li>
+						<li class="page-item"><a class="page-link" href="#">2</a></li>
+						<li class="page-item"><a class="page-link" href="#">3</a></li>
+						<li class="page-item">
+						  <a class="page-link" href="#">Next</a>
+						</li>
+					  </ul>
+					</nav>
+				</div>
+				
+				<div class="col-md-1">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+</div>
