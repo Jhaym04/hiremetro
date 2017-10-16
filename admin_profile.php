4ea04a1b298cee869e8704aa85645daf5b60b75f
Lei Dian <31701658+leidianm@users.noreply.github.com>
2017-10-16 21:59:22 +0800

    Add files via upload
    profile.

---
+++ admin_profile.php
@@ -0,0 +1,113 @@
+
+
+<div class="container-fluid pagee" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+	
+	<br/>
+	<br/>
+	<br/>
+	
+	<div class="row">
+        <div class="col-sm-3 col-md-2 sidebar" >
+          <ul class="nav nav-sidebar">
+			<br/>
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-th-list"> Dashboard</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"> Profiles</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"> Reports</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-check"> Suggestions</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-wrench"> Settings</a></li>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+			<p>hiremetro © 2017 | Designed by Group 4</p>
+          </ul>
+		</div>
+	</div>
+	<div class="row" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+		<div class="col-md-2">
+		</div>
+		<div class="col-md-10" >
+			<div class="row">
+				<div class="col-md-11">
+					<br />
+					<div class="row">
+						<div class="col-md-1">
+							<img class="img-responsive" src="<?php echo base_url('images/profile.png')?>" style="opacity:0.75;" >
+						</div>
+						<div class="col-md-10">
+							<h1>Profiles</h1>
+						</div>
+					</div>
+					
+					<hr style="border: solid 2px black; opacity:0.50;"/>
+					
+					<div class="row">
+						<div class="col-md-12">
+							<table class="table table-striped">
+								<thead>
+								  <tr class="success">
+									<th>Name</th>
+									<th>Address</th>
+									<th>Job</th>
+									<th>E-mail</th>
+									<th>Contact No.</th>
+									<th>Action</th>
+								  </tr>
+								</thead>
+								<tbody>
+									<tr>
+										<td>name_samp</td>
+										<td>add_samp</td>
+										<td>job_samp</td>
+										<td>email_samp</td>
+										<td>contact_samp</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>
+									<tr>
+										<td>name_samp</td>
+										<td>add_samp</td>
+										<td>job_samp</td>
+										<td>email_samp</td>
+										<td>contact_samp</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>
+									<tr>
+										<td>name_samp</td>
+										<td>add_samp</td>
+										<td>job_samp</td>
+										<td>email_samp</td>
+										<td>contact_samp</td>
+										<td>
+											<a href="">View</a> |
+											<a href="">Delete</a>
+										</td>
+									</tr>									  
+								</tbody>
+							</table>
+						</div>
+					</div>
+					
+					<br />
+					<br />
+				</div>
+				
+				<div class="col-md-1">
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+	
+	
+</div>
