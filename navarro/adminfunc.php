727a2c5f089591342509f372c53ff5c5d8b33ea4
navarrochristianjames <31690292+navarrochristianjames@users.noreply.github.com>
2017-10-17 22:53:37 +0800

    Add files via upload

---
+++ navarro/adminfunc.php
@@ -0,0 +1,165 @@
+<nav class="navbar navbar-custom navbar-fixed-top">
+        <div class="navbar-header" id="logo" >
+                <img src="<?php echo base_url('images/logo.png')?>" >
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span> 
+                </button>
+        </div>  
+
+        
+        <div class="collapse navbar-collapse" id="myNavbar">
+        
+            <ul class="nav navbar-nav navbar-right">
+                 <li><a href="#"><span class="glyphicon glyphicon-home"  ></span> Home </a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-user"  ></span> Log Out </a></li>
+               
+            </ul>
+        </div>
+    </nav>
+<script>
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
+</script>
+
+<div class="container-fluid pagee" style="background-image: url('<?php echo base_url('images/bg_admin.png')?>');">
+    
+    <br/>
+    <br/>
+    <br/>
+       <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar" >
+          <ul class="nav nav-sidebar">
+            <br/>
+            <li class="active"><a href="#"><span class="glyphicon glyphicon-th-list"> Dashboard</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-user"> Profiles</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-info-sign"> Reports</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-check"> Suggestions</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-wrench"> Settings</a></li>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <p>hiremetro © 2017 | Designed by Group 4</p>
+          </ul>
+        </div>
+    
+ 
+
+        <div class="col-md-2">
+        </div>
+        <div class="col-md-10">
+
+            
+     <br /> 
+       <br />
+
+      </div>
+           <div class="col-md-2">
+        </div>
+        <div class="col-xs-10" >
+   
+   
+          <div class="panel panel-info">
+            <div class="panel-heading">
+              <h3 class="panel-title">Navarro Christian James Navarro</h3>
+            </div>
+            <div class="panel-body">
+              <div class="row">
+                <div class="col-md-3 col-lg-3 " align="center"> <img alt="User Pic"   src="<?php echo base_url('images/logo.png')?>" class="img-circle img-responsive"> </div>
+                
+               
+                <div class=" col-md-9 col-lg-9 "> 
+                        
+                  <table class="table table-user-information">
+                 
+                    <tbody>
+                      <tr>
+                        <td>ID:</td>
+                        <td>12345 </td>
+                      </tr>
+                      <tr>
+                        <td>First Name:</td>
+                        <td>christian </td>
+                      </tr>
+                      <tr>
+                        <td>Middle Name</td>
+                        <td>doyola</td>
+                      </tr>
+                   
+                         <tr>
+                             <tr>
+                        <td>Last Name</td>
+                        <td>Navs</td>
+                      </tr>
+                        <tr>
+                        <td>Home Address</td>
+                        <td>sapnu,puas</td>
+                      </tr>
+                      <tr>
+                        <td>Birthday</td>
+                        <td> 5/16/1998</td>
+                      </tr>
+                        <td>Sex</td>
+                        <td> Male
+                        </td>
+                           
+                      </tr>
+                      <tr>
+                      </div>
+
+                    
+                        <td>Email</td>
+                        <td><a href="navarrochristianjames@gmail.com">navarrochristianjames@gmail.com</a></td>
+                      </tr>
+                        <td>Phone Number</td>
+                        <td>123-4567-890(Landline)555-4567-890(Mobile)
+                        </td>
+                      </tr>
+                        <tr>
+                        <td>Work Title</td>
+                        <td>dancer </a></td>
+                      </tr>
+                        <td> Work Description</td>
+                        <td>macho
+                        </td>
+                      </tr>  <tr>
+                        <td>Work Pay</td>
+                        <td>120000</td>
+                      </tr>
+                        <td>Work Location</td>
+                        <td> Pasay
+                        </td>
+                      </tr>  <tr>
+                        <td>Work Language</td>
+                        <td>Tgalaog</td>
+                      </tr>
+                        <td>Date Posted</td>
+                        <td>1/12/2016)
+                        </td>
+                      </tr>
+                       </tr>
+                        <td>Status</td>
+                        <td>1/12/2016)
+                        </td>
+                      </tr>
+                           
+                     
+
+                    </tbody>
+                  </table>
+                  
+               
+            
+          </div>
+        </div>
+      </div>
+    </div>
