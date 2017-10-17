32be13f3998d89b00480e91210bbdbd1b3d759f0
maw0904 <31690512+rarkzem@users.noreply.github.com>
2017-10-17 13:11:50 +0800

    Modal for View

---
+++ Czar(commits)/modal_suggestions.php
@@ -0,0 +1,39 @@
+<!-- Modal for view suggestions-->
+		<div class="modal fade" id="viewsuggestion" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content" id="login">
+					<div class="modal-header text-center" id="header">
+						<button type="button" class="close" data-dismiss="modal"> &times; </button>
+						<h4 class="modal-title"> View Suggestions </h4>
+						<p>Read more</p>
+					</div>
+					<div class="modal-body ">
+						<div class="row">
+							<div class="col-md-12">
+								<h4>Subject Line: _______________</h4>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12">
+								<h5>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+								Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h5>
+							</div>
+						</div>
+						<div class="row" style="text-align:right">
+							<div class="col-md-12">
+								<h5>10-15-2017</h5>
+							</div>
+						</div>
+					</div>
+					<div class="modal-footer text-center">
+						<button class="btn btn-info" data-dismiss="modal">Back</button>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		
+		
