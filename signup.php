e22aea0c76a501443853e984e93f0e011c062c9d
ecmariin <31694078+ecmariin@users.noreply.github.com>
2017-10-22 21:46:55 +0800

    sign up ver2

--- signup.php
+++ signup.php
@@ -1,93 +1,18 @@
 
-<style>
-
-div {
-	font-family: montserrat;
-}
-
-#scroll {
-	width: 364.6px;
-	height: 622px;
-	overflow-y: scroll;
-}
-
-#borderleft{
-	border-left: 2px solid lightgray;
-	padding-left: 35px;
-	
-}
-
-#padd { 
-padding-right:20px;
-}
-
-#paddd{
-	padding-right: 35px;
-}
-
-.badge {
-	background-color: #3b8686;
-}
-
-#head {
-	padding-top: 10px;
-	font-family: montserrat;
-	font-size: 30px;
-	color: #3b8686;
-}
-#content {
-	padding-top: 10px;
-	padding-left: 20px;
-	font-family: montserrat;
-	font-size: 20px;
-}
-
-.fix-div {position:fixed; height:100%; width: 100%; left 0%; top:0%; z-index:1;}
-#wrapper {position:relative; z-index:2; margin:100%; 0 0}
-
-#padds {
-	padding-right: 20px;
-}
-
-#sign {
-    background-color: #3b8686;
-    color: white;
-	border-radius: 10px;
-    padding: 0px 50px;
-    text-align: center;
-    font-size: 22px;
-}
-
-.name {height:40px; width: 210px; margin: 2px; padding: 5px; font-size: 18px;}
-.info {height:40px; width: 429px; margin: 2px; padding: 5px; font-size: 18px;}
-
-
-</style>
 
 <!-- filter-->
 	
-	<!-- Navbar -->
-		
-	<nav class="navbar navbar-custom navbar-fixed-top">
-		<div class="navbar-header" id="logo" >
-				<img src="<?php echo base_url('images/logo.png')?>">
-				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-
-				</button>
-		</div>	
-		
-		<div class="collapse navbar-collapse" id="myNavbar">
-		
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Login</a></li>
-				<li><a href="#">Join Us</a></li>
-				
-			</ul>
-		</div>
-	</nav>
-
+	<?php
+		if(!empty(validation_errors())){
+			echo "<script> 
+			 $(window).on('load',function(){
+				$('#form_error').modal('show');
+			});
+		</script>";
+		}
+	?>
 	
-	<div class="container-fluid pagee" style="background-image: url('images/bg.png'); min-height: 500px; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; ">
+	<div class="container-fluid pagee" style="background-image: url(<?php echo base_url('images/bg.png')?>); min-height: 500px; background-attachment: fixed; background-position: center; background-repeat: no-repeat; background-size: cover; ">
 	<br />
 	<br />
 	<br />
@@ -109,11 +34,13 @@ padding-right:20px;
 						</ul>
 					</div>
 					
+					<br/>
+					
 					<div class="row" style="position: relative; top: -30px;">
 				
 						<div class="col-md-11">
-							<div class="row" style="padding-left: 30px;">
-								<h1>Join Us</h1>
+							<div class="row" style="padding-left: 30px;" id="head">
+								<h1><b>Join Us</b></h1>
 							</div>
 							
 							<div class="row" style="padding-left: 20px;">
@@ -122,25 +49,24 @@ padding-right:20px;
 							</div>	
 							
 							<div class="row" style="padding-left: 70px;">
-								<form action="#">
-								  <input class="name" type="text" name="firstname" placeholder="First Name">
-								  <input class="name" type="text" name="lastname" placeholder="Last Name"> <br />
-								  <input class="info" type="text" name="email" placeholder="Email"> <br />
-								  <input class="info" type="password" name="password" placeholder="Password"> <br />
-								</form>
+								<?php echo form_open('hiremetro/signup'); ?>
+								  <input class="name" type="text" name="fname" placeholder="First Name" value="<?php echo set_value('fname'); ?>">
+								  <input class="name" type="text" name="lname" placeholder="Last Name" value="<?php echo set_value('lname'); ?>"> <br />
+								  <input class="info" type="email" name="email" placeholder="Email" value="<?php echo set_value('email'); ?>"> <br />
+								  <input class="info" type="password" name="password" placeholder="Password" value="<?php echo set_value('password'); ?>"> <br />
+								  <input class="info" type="password" name="confirm_password" placeholder="Confirm Password" > <br />
 							</div>
 							
 							<div class="row" style="padding-left: 70px; padding-right: 70px;">
 								<br />
-								<h5>By clicking Create Account, you agree to our <u style="color: blue;">Terms of Service</u>. You may receive e-mail from Hiremetro at any time.</h5>
+								<h5>Before clicking Sign up please make sure you agree to our <a style="text-decoration: none; cursor: pointer;" data-toggle="modal" data-target="#termsofservModal">Terms of Service</a>. You may receive e-mail from Hiremetro at any time.</h5>
 							</div>
 
 							<div class="row" style="padding-left: 100px;">
-								<form action="#">
-								  <input type="checkbox" name="name" value="value"> I agree to terms of service of Hiremetro.</input>
+								  <label><input type="checkbox"  onchange="document.getElementById('sign').disabled = !this.checked;" /> I agree to terms of service of Hiremetro.</input>
 								  <br /> <br />
-								  <input type="submit" value="Sign Up" id="sign">
-								</form>
+								  <input type="submit" value="Sign Up" id="sign" name="signup" disabled>
+								  </form>
 							</div>	
 							
 						</div>
@@ -155,7 +81,9 @@ padding-right:20px;
 				
 				
 				<!--MALIIT-->
-					
+				
+				</br>
+				
 				<div id="right" class="col-md-5" id="borderleft">
 
 					<div class="row" >
@@ -186,41 +114,24 @@ padding-right:20px;
 			</div>
 			
 		</div>
-		<br />
-
-		<!--footer-->
 		
-		<div class="row" id="footer">
-			
-			<div class="col-lg-1">
+		<!-- Modal for Form Validation Error-->
+		<div class="modal fade" id="form_error" role="dialog">
+			<div class="modal-dialog">
+				<div class="modal-content" id="login">
+					<div class="modal-header text-center" id="header">
+						<button type="button" class="close" data-dismiss="modal"> &times; </button>
+						<h4 class="modal-title"> SIGN UP </h4>
+					</div>
+					<div class="modal-body text-center">
+						<?php echo validation_errors(); ?>
+					</div>
+					<div class="modal-footer text-center">
+						<button class="w3-btn w3-hover-teal w3-black" data-dismiss="modal">Ok</button>
+						</form>
+					</div>
+				</div>
 			</div>
-			<div class="col-lg-1">
-				</br>
-				<a>About Us</a>
-				<br /><a>Comments</a>
-				<br /><a>Suggestions</a>
-				<br />
-			</div>
-			<div class="col-lg-1">
-				</br>
-				<a>Special Offer</a>
-				<br /><a>Discount</a>
-				<br /><a>Freebies</a>
-				<br />
-			</div>
-			<div class="col-lg-6 text-center">
-				<br />
-				<p>hiremetro © 2017 | Designed by Group 4</p>
-			</div>
-			<div class="col-lg-3 ">
-				<br />
-				<p><img src="<?php echo base_url('images/fb.ico')?>" alt=""> facebook.com/hiremetro</p>
-				<p><img src="<?php echo base_url('images/twitter.png')?>" alt=""> @hiremetro</p>
-				<p><img src="<?php echo base_url('images/gmail.ico')?>" alt=""> hiremetro@gmail.com</p> 
-			</div>
-			<div class="col-lg-1">
-			</div>
-			
 		</div>
+
 		
-	</div>
